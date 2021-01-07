54abcfe55722e82adaaf8da46cdef89637ed8a4a
unknown <andrezasecon@gmail.com>
2021-01-06 22:15:44 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+import { ReactComponent as IconYoutube} from './youtube.svg';
+import { ReactComponent as IconLinkedin } from './linkedin.svg';
+import { ReactComponent as IconIntagram } from './instagram.svg';
+
+function Footer(){
+    return (    
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <IconYoutube />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <IconLinkedin />
+                </a>
+                <a href="https://www.istagram.com/devsuperior.ig" target="_new">
+                    <IconIntagram />
+                </a>
+
+            </div>
+        </footer>
+    
+    )
+}
+
+export default Footer;
