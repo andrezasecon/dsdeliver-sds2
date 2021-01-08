3124a93b62b9a6cc06a5f225368e2582506c0e8e
unknown <andrezasecon@gmail.com>
2021-01-07 22:52:05 -0300

    add Order cards and dynamic lists

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,14 +1,29 @@
-import StepsHeader from './StepsHeader';
 import './styles.css';
+import StepsHeader from './StepsHeader';
+import ProductsList from "./ProductsList";
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fethProducts } from '../api';
 
 function Orders(){
+
+const [products, setProducts] = useState<Product[]>([]);
+
+useEffect(() => {
+    fethProducts()
+    .then(response => setProducts(response.data))
+    .catch(error => console.log(error))
+}, []);
+
+
     return (    
     
             <div className="orders-container">
-                <StepsHeader />                
+                <StepsHeader />  
+                <ProductsList products={products} />
             </div>
     
-    )
+    );
 }
 
 export default Orders;
