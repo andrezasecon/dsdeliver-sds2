725d8354c9ff4746a9c3985d675d0958a25c832c
unknown <andrezasecon@gmail.com>
2021-01-09 01:27:38 -0300

    Search Ordem Location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,12 +2,15 @@ import './styles.css';
 import StepsHeader from './StepsHeader';
 import ProductsList from "./ProductsList";
 import { useEffect, useState } from 'react';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 import { fethProducts } from '../api';
+import OrderLocation from './OrderLocation';
+
 
 function Orders(){
 
 const [products, setProducts] = useState<Product[]>([]);
+const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
 useEffect(() => {
     fethProducts()
@@ -21,9 +24,10 @@ useEffect(() => {
             <div className="orders-container">
                 <StepsHeader />  
                 <ProductsList products={products} />
+                <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
             </div>
     
-    );
+    )
 }
 
 export default Orders;
